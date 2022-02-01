aee102e441f41a3da1053088bd81df7ee309847e
yamadakotaro <yamadakotaro@team-lab.com>
2022-02-02 00:06:13 +0900

    [add]実行ファイル追加

--- bin/aws-networking-workshop-cdk.ts
+++ bin/aws-networking-workshop-cdk.ts
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AwsNetworkingWorkshopCdkStack } from '../lib/aws-networking-workshop-cdk-stack';
+import { App } from 'aws-cdk-lib';
+import { NetworkStack } from '../lib/network-stack';
+import { SubnetType } from 'aws-cdk-lib/aws-ec2';
 
-const app = new cdk.App();
-new AwsNetworkingWorkshopCdkStack(app, 'AwsNetworkingWorkshopCdkStack', {
-});
+const app = new App();
+new NetworkStack(app, 'network', {
+  vpcProps: {
+    vpcName: 'VPC A',
+    cidr: '10.0.0.0/16',
+    subnetConfiguration: [
+      {
+        cidrMask: 24,
+        name: 'VPC A - AZ1',
+        subnetType: SubnetType.PUBLIC
+      }
+    ]
+  }
+})
