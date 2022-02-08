807d48351cde32b7d94aa136e8a56183cc5f9d72
yamadakotaro <yamadakotaro@team-lab.com>
2022-02-09 00:31:08 +0900

    [fix]trantitGatewayを生成するよう実行ファイル修正

--- bin/aws-networking-workshop-cdk.ts
+++ bin/aws-networking-workshop-cdk.ts
@@ -3,41 +3,51 @@ import { App } from "aws-cdk-lib";
 import "source-map-support/register";
 import { NetworkStack } from "../lib/network-stack";
 import { ServerStack } from "../lib/server-stack";
+import { TransitGatewayStack } from '../lib/trangit-gateway-stack';
 
 const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT,
   region: process.env.CDK_DEFAULT_REGION,
 };
 
+const vpcACiderBlock = "10.0.0.0/16"
+const vpcBCiderBlock = "10.1.0.0/16"
+const vpcCCiderBlock = "10.2.0.0/16"
+
 const app = new App();
 
 const network1 = new NetworkStack(app, "network1", {
   env,
   vpcName: "VPC A",
-  cidr: "10.0.0.0/16",
+  cidr: vpcACiderBlock,
 });
 const network2 = new NetworkStack(app, "network2", {
   env,
   vpcName: "VPC B",
-  cidr: "10.1.0.0/16",
+  cidr: vpcBCiderBlock,
 });
 const network3 = new NetworkStack(app, "network3", {
   env,
   vpcName: "VPC C",
-  cidr: "10.2.0.0/16",
+  cidr: vpcCCiderBlock,
 });
 
-const server1 = new ServerStack(app, "server1", {
+new TransitGatewayStack(app, "trangitGateway", {
+  vpcs: [network1.vpc, network2.vpc, network3.vpc]
+})
+
+new ServerStack(app, "server1", {
   env,
   vpc: network1.vpc,
+  allowCiderBlocks: [vpcBCiderBlock, vpcCCiderBlock]
 });
-const server2 = new ServerStack(app, "server2", {
+new ServerStack(app, "server2", {
   env,
   vpc: network2.vpc,
-  allowCiderBlocks: [server1.publicIpAdress + "/32"],
+  allowCiderBlocks: [vpcACiderBlock, vpcCCiderBlock],
 });
-const server3 = new ServerStack(app, "server3", {
+new ServerStack(app, "server3", {
   env,
   vpc: network3.vpc,
-  allowCiderBlocks: [server1.publicIpAdress + "/32", server2.publicIpAdress + "/32"],
+  allowCiderBlocks: [vpcACiderBlock, vpcBCiderBlock],
 });
