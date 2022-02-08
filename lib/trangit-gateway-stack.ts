abf7b67431ac8ac96e2b71c2560fe9cefe9b0d86
yamadakotaro <yamadakotaro@team-lab.com>
2022-02-09 00:24:59 +0900

    [fix]サブネットのルートテーブルの向き先にTGWを追加

--- lib/trangit-gateway-stack.ts
+++ lib/trangit-gateway-stack.ts
@@ -1,5 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { CfnTransitGateway, CfnTransitGatewayAttachment, Vpc, SubnetType, Subnet } from 'aws-cdk-lib/aws-ec2';
+import { CfnTransitGateway, CfnTransitGatewayAttachment, Vpc, SubnetType, Subnet, CfnRoute } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 
 export interface TransitGatewayProps extends StackProps {
@@ -10,32 +10,44 @@ export interface TransitGatewayProps extends StackProps {
  * transit gateway stack
  */
 export class TransitGatewayStack extends Stack {
+  readonly transitGatewayId: string;
+
   constructor(scope: Construct, id: string, props: TransitGatewayProps) {
     super(scope, id, props);
 
     const transitGateway = new CfnTransitGateway(this, 'transitGateway', {
       description: 'Immersion Day TGW'
     })
+    this.transitGatewayId = transitGateway.attrId;
 
-    props.vpcs.forEach((vpc, index) => {
+    props.vpcs.forEach((vpc, vpcIndex) => {
       // transit gateway用のsubnetを2つ作成
-      const subnet1ForTGW = new Subnet(this, 'subnet1ForTGW', {
+      const subnet1ForTGW = new Subnet(this, `subnet1ForTGW-vpc${vpcIndex}`, {
         availabilityZone: 'ap-northeast-1a',
         vpcId: vpc.vpcId,
-        cidrBlock: `10.${index}.2.0/28` 
+        cidrBlock: `10.${vpcIndex}.2.0/28` 
       })
 
-      const subnet2ForTGW = new Subnet(this, 'subnet2ForTGW', {
+      const subnet2ForTGW = new Subnet(this, `subnet2ForTGW-vpc${vpcIndex}`, {
         availabilityZone: 'ap-northeast-1c',
         vpcId: vpc.vpcId,
-        cidrBlock: `10.${index}.3.0/28` 
+        cidrBlock: `10.${vpcIndex}.3.0/28` 
       })
 
       // transit gateway attachmentを作成
-      new CfnTransitGatewayAttachment(this, 'transitGatewayAttachmentA', {
+      new CfnTransitGatewayAttachment(this, `transitGatewayAttachment-vpc${vpcIndex}`, {
         transitGatewayId: transitGateway.attrId,
         vpcId: vpc.vpcId,
         subnetIds: [subnet1ForTGW.subnetId, subnet2ForTGW.subnetId]
+      })
+
+      // サブネットのルートテーブルの向き先にTGWを追加
+      vpc.publicSubnets.forEach((publicSubnet, subnetIndex) => {
+        new CfnRoute(this, `routeForTGW-vpc${vpcIndex}-subnet${subnetIndex}`,{
+          routeTableId: publicSubnet.routeTable.routeTableId,
+          destinationCidrBlock: '10.0.0.0/8',
+          transitGatewayId: this.transitGatewayId
+        })
       })
     })
   }
